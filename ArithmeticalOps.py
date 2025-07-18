6c57cdb34693430208aabd50a0d79a01aad3f286
yaduvanshi2025 <manishyadav0771@gmail.com>
2025-07-18 16:20:51 +0530

    operation

---
+++ ArithmeticalOps.py
@@ -0,0 +1,16 @@
+#Write a Python Program to do arithmetical Operations Addition and Division
+
+#Addition
+
+num1=float(input("Enter the First Number For Addition:"))
+num2=float(input("Enter the Second Number For Addition:"))
+Sum_result=num1+num2
+print(f"Sum:{num1}+{num2}={Sum_result}")
+
+
+#Division
+
+num3=float(input("Enter the First Number For Division:"))
+num4=float(input("Enter the Second Number For Division:"))
+Sum_result=num3/4
+print(f"Sum:{num3}/{num4}={Sum_result}")
